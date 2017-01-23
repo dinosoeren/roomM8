97d382547655ff4ad2fc02aaf96b12c6ac132044
SWalls <soerenwalls@gmail.com>
2017-01-22 21:21:52 -0700

    Enabled gzip compression and js/css minifying.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const auth = require('./config/auth');
 
 // Primary app dependencies.
 const flash = require('connect-flash');
+const compression = require('compression');
+const minify = require('express-minify');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,8 +25,6 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 // Use Embedded Javascript as the template engine.
 app.set('view engine', 'ejs');
-// Always serve files in public (css, js, images, etc).
-app.use(express.static('public'));
 // Use flash to send messages over middleware.
 app.use(flash());
 // Init session for passport authentication library.
@@ -32,9 +32,24 @@ app.use(session(auth));
 app.use(passport.initialize());
  // Persistent login sessions.
 app.use(passport.session());
+ // Enable compression for faster response times.
+app.use(compression());
 
 // Load routes: pass in app, auth info, and fully configured passport.
 require('./app/routes')(app, auth, passport);
+
+// Minify and cache CSS and JS files.
+app.use(function(req, res, next)
+{
+    // for all *.min.css or *.min.js, do not minify it 
+    if (/\.min\.(css|js)$/.test(req.url)) {
+        res._no_minify = true;
+    }
+    next();
+});
+app.use(minify());
+// Always serve files in public (css, js, images, etc).
+app.use(express.static(__dirname + '/public'));
 
 // Launch
 app.listen(port);
